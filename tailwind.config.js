613c475ffd2dd4791924b1b41b858eba7796a18c
kevinandreanus <kevin@meestakes.com>
2021-05-15 18:55:00 +0700

    student-setting-edit.html complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins"],
       },
+      fontSize: {
+        'xxs': ['10px', '14px'],
+      },
       colors: {
         hitam: "#251526",
         merah: "#CE272D",
@@ -17,6 +20,13 @@ module.exports = {
         searchbar: "875px",
         iphone5: "321px",
       },
+      inset: {
+        '50': "198px",
+        '34': "8.5rem",
+        '33': "132px",
+        '22': "94px",
+        '23': "95px",
+      }
     },
   },
   variants: {
